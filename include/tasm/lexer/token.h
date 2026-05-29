52604bbfbf66954066ce68a6027e34aee60f8053
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-05-29 14:59:26 +0200

    feat(lexer): add token definitions

---
+++ include/tasm/lexer/token.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <tasm/defs/sv.h>
+
+typedef enum TokenType {
+    TT_EOF,
+    
+    TT_DOT,     /// .
+    TT_COMMA,   /// ,
+    TT_IDENT,   /// anything 
+    TT_COLON,   /// :
+    TT_NEWLINE, /// \n
+
+    TT_IMM_INT,  /// 123
+    TT_IMM_CHAR, /// 'x'
+    TT_IMM_STR,  /// "anything"
+
+    TT_KW_OP_NOP,   /// nop
+    TT_KW_OP_HALT,  /// halt
+    TT_KW_OP_MIN,   /// min
+    TT_KW_OP_MAX,   /// max
+    TT_KW_OP_ROT,   /// rot
+    TT_KW_OP_SHL,   /// shl
+    TT_KW_OP_SHR,   /// shr
+    TT_KW_OP_NOT,   /// not
+    TT_KW_OP_ADD,   /// add
+    TT_KW_OP_SUB,   /// sub 
+    TT_KW_OP_UMUL,  /// umul
+    TT_KW_OP_UDIV,  /// udiv
+    TT_KW_OP_SMUL,  /// smul
+    TT_KW_OP_SDIV,  /// sdiv
+    TT_KW_OP_IN,    /// in
+    TT_KW_OP_OUT,   /// out
+    TT_KW_OP_LOAD,  /// load
+    TT_KW_OP_STORE, /// store
+    TT_KW_OP_SET,   /// set
+    TT_KW_OP_INC,   /// inc 
+    TT_KW_OP_DEC,   /// dec
+    TT_KW_OP_CMP,   /// cmp
+    TT_KW_OP_JMP,   /// jmp
+} TokenType;
+
+typedef struct Token {
+    TokenType  type;
+    StringView lexeme;
+} Token;
