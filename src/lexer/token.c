47cf3e0ff081b1c827e09918773f23d35206ebe1
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-06-06 13:51:54 +0200

    fix(token): remove keyword token types from .c file too

--- src/lexer/token.c
+++ src/lexer/token.c
@@ -14,32 +14,6 @@ static StringView token_type_to_string_map[] = {
     [TT_IMM_INT]     = SV("IMM_INT"),
     [TT_IMM_CHAR]    = SV("IMM_CHAR"),
     [TT_IMM_STR]     = SV("IMM_STR"),
-
-    [TT_KW_IF]       = SV("KW_IF"),
-
-    [TT_KW_OP_NOP]   = SV("KW_OP_NOP"),
-    [TT_KW_OP_HALT]  = SV("KW_OP_HALT"),
-    [TT_KW_OP_MIN]   = SV("KW_OP_MIN"),
-    [TT_KW_OP_MAX]   = SV("KW_OP_MAX"),
-    [TT_KW_OP_ROT]   = SV("KW_OP_ROT"),
-    [TT_KW_OP_SHL]   = SV("KW_OP_SHL"),
-    [TT_KW_OP_SHR]   = SV("KW_OP_SHR"),
-    [TT_KW_OP_NOT]   = SV("KW_OP_NOT"),
-    [TT_KW_OP_ADD]   = SV("KW_OP_ADD"),
-    [TT_KW_OP_SUB]   = SV("KW_OP_SUB"),
-    [TT_KW_OP_UMUL]  = SV("KW_OP_UMUL"),
-    [TT_KW_OP_UDIV]  = SV("KW_OP_UDIV"),
-    [TT_KW_OP_SMUL]  = SV("KW_OP_SMUL"),
-    [TT_KW_OP_SDIV]  = SV("KW_OP_SDIV"),
-    [TT_KW_OP_IN]    = SV("KW_OP_IN"),
-    [TT_KW_OP_OUT]   = SV("KW_OP_OUT"),
-    [TT_KW_OP_LOAD]  = SV("KW_OP_LOAD"),
-    [TT_KW_OP_STORE] = SV("KW_OP_STORE"),
-    [TT_KW_OP_SET]   = SV("KW_OP_SET"),
-    [TT_KW_OP_INC]   = SV("KW_OP_INC"),
-    [TT_KW_OP_DEC]   = SV("KW_OP_DEC"),
-    [TT_KW_OP_CMP]   = SV("KW_OP_CMP"),
-    [TT_KW_OP_JMP]   = SV("KW_OP_JMP"),
 };
 
 StringView tasm_token_type_to_string(TasmTokenType tt) {
