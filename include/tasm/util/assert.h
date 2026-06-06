bb7d8479e59730f5d68fbc2ef15b726412d82e6f
Maqi-x <186316794+Maqi-x@users.noreply.github.com>
2026-06-06 09:29:15 +0200

    feat(util): add assert macros

---
+++ include/tasm/util/assert.h
@@ -0,0 +1,52 @@
+#include <stdio.h>  // NOLINT(misc-include-cleaner)
+#include <stdlib.h> // NOLINT(misc-include-cleaner)
+
+#if defined(__GNUC__) || defined(__clang__)
+    #define TASM_UNREACHABLE(STR) do { \
+        fprintf(stderr, "REACHED UNREACHABLE: %s (%s:%d)\n", STR, __FILE__, __LINE__); \
+        __builtin_unreachable(); \
+    } while (0)
+#elif defined(_MSC_VER)
+    #define TASM_UNREACHABLE(STR) do { \
+        fprintf(stderr, "REACHED UNREACHABLE: %s (%s:%d)\n", STR, __FILE__, __LINE__); \
+        __assume(0); \
+    } while (0)
+#else
+    #define TASM_UNREACHABLE(STR) do { \
+        fprintf(stderr, "REACHED UNREACHABLE: %s (%s:%d)\n", STR, __FILE__, __LINE__); \
+        abort(); \
+    } while (0)
+#endif
+
+#if defined(__GNUC__) || defined(__clang__)
+    #define TASM_UNREACHABLE_ENUM_VAL(TYPE, VAL) do { \
+        fprintf(stderr, "invalid or not handled value %d for enum %s (%s:%d)\n", \
+            (int)(VAL), #TYPE, __FILE__, __LINE__); \
+        __builtin_unreachable(); \
+    } while (0)
+#elif defined(_MSC_VER)
+    #define TASM_UNREACHABLE_ENUM_VAL(TYPE, VAL) do { \
+        fprintf(stderr, "invalid or not handled value %d for enum %s (%s:%d)\n", \
+            (int)(VAL), #TYPE, __FILE__, __LINE__); \
+        __assume(0); \
+    } while (0)
+#else
+    #define TASM_UNREACHABLE_ENUM_VAL(TYPE, VAL) do { \
+        fprintf(stderr, "invalid or not handled value %d for enum %s (%s:%d)\n", \
+            (int)(VAL), #TYPE, __FILE__, __LINE__); \
+        abort(); \
+    } while (0)
+#endif
+
+#ifdef TASM_DEBUG
+    #define TASM_ASSERT(EXPR, MSG) do { \
+        if (!(EXPR)) { \
+            fprintf(stderr, "ASSERTION FAILED: %s\nMessage: %s\nLocation: %s:%d\n", \
+                #EXPR, MSG, __FILE__, __LINE__); \
+            abort(); \
+        } \
+    } while (0)
+#else
+    #define TASM_ASSERT(EXPR, MSG) ((void)0)
+#endif
+
